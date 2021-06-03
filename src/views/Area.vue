f2b1e1b44995951a270dd3d1edaff6bcf908731c
sly <atomicagression@gmail.com>
2021-06-03 11:12:24 +0300

    extra

--- src/views/Area.vue
+++ src/views/Area.vue
@@ -1,6 +1,33 @@
 <template>
-	<div>
-		I'm here now!
+	<div class="area">
+		<div class="container">
+			<div class="main">
+				<div class="panel">
+					<div class="panel-body">
+						<h3>Tests</h3>
+						<hr>
+						I'm here now!
+						<br><br>
+						<h3>Users</h3>
+						<hr>
+					</div>
+				</div>
+			</div>
+			<div class="sidebar">
+				<div class="panel">
+					<div class="panel-body">
+						<h3>Actions</h3>
+						<hr>
+						<div>
+							<a>New test</a>
+						</div>
+						<div>
+							<a>New user</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -38,4 +65,39 @@
 </script>
 
 <style lang="scss">
+	h1, h2, h3 {
+		margin: 0px;
+	}
+
+	hr {
+		border: 1px solid #dedede;
+	}
+
+	.area {
+		display: flex;
+		padding: 10px;
+
+		.container {
+			display: flex;
+		}
+
+		.main {
+			flex-grow: 1;
+			margin-right: 10px;
+		}
+
+		.sidebar {
+			width: 200px;
+		}
+
+		.panel {
+			width: 100%;
+		}
+	}
+
+	.container {
+		width: 900px;
+		max-width: calc(100% - 30px);
+		margin: auto;
+	}
 </style>
