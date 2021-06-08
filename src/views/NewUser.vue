650ef1e605b0ae0a6ef793e9f6731a553ddd5d60
sly <atomicagression@gmail.com>
2021-06-08 17:33:40 +0300

    user creation

--- src/views/NewUser.vue
+++ src/views/NewUser.vue
@@ -6,11 +6,12 @@
           <div class="panel-body">
             <h3>Create a new user</h3>
             <hr>
-            <span></span>
+            <span class="notif" v-if="success">Successfully created a new user</span>
+            <span class="notif" v-if="error">Failed to create a new user</span>
             <form class="verform">
-              <input type="text" name="Username" placeholder="Username">
-              <input type="text" name="Password" placeholder="Password">
-              <input type="submit" value="Submit">
+              <input type="text" name="Username" placeholder="Username" ref="username">
+              <input type="password" name="Password" placeholder="Password" ref="password">
+              <input type="submit" value="Submit" v-on:click.stop="addUser">
             </form>
           </div>
         </div>
@@ -27,7 +28,49 @@
 export default {
   name: 'NewUser',
   methods: {
-    login() {
+    addUser(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if(this.$refs.username.value != "" && this.$refs.password.value != "") {
+        const requestOptions = {
+          method: "POST",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: this.$refs.username.value,
+            password: this.$refs.password.value
+          })
+        };
+
+        console.log("Sending: ", requestOptions);
+
+        fetch(`http://localhost:3000/adduser`, requestOptions)
+          .then(response => response.json())
+          .then(data => {
+            console.log("Sent");
+
+            if (data.success) {
+              this.error = false;
+              this.success = true;
+            }
+            else {
+              this.error = true;
+              this.success = false;
+            }
+          });
+      }
+      else {
+          console.log("A username and password must be present");
+      }
+    }
+  },
+  data() {
+    return {
+      success: false,
+      error: false
     }
   }
 }
@@ -36,6 +79,12 @@ export default {
 <style lang="scss" scoped>
   h1, h2, h3 {
     margin: 0px;
+  }
+
+  .notif {
+    color:#656565;
+    margin-bottom: 5px;
+    display: inline-flex;
   }
 
   .text-center {
